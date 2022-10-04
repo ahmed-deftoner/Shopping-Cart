aae6394862d3192b704ee6f170afa52c877e196d
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-10-04 18:52:20 +0500

    guitar data class

---
+++ app/src/main/java/Models/Guitars.kt
@@ -0,0 +1,11 @@
+package Models
+
+data class Guitars(
+    val name: String,
+    val imgSrc: String,
+    val price: Float,
+    val description: String,
+    val scaleLength: Float,
+    val body: String,
+    val rating: Float
+)
