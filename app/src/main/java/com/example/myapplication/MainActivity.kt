d7ae6b5fab0ee233a24023e9a2dbe9e9fd52d55f
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-12-06 23:37:33 +0500

    notification done

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,27 +1,49 @@
 package com.example.myapplication
 
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.ImageView
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.myapplication.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
-    var btnAddtoCart: Button? = null
+    val CHANNEL_ID = "23"
+    val CHANNEL_NAME = "cartNotify"
+    val NOTIF_ID = 0
 
+    private fun createNotifChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT).apply {
+                lightColor = Color.BLUE
+                enableLights(true)
+            }
+            val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val intent = intent
+        createNotifChannel()
         val name = "Stinger"
         val price = 2300
         val image = R.drawable.gfg
-        val id = 34
-        val desc_id = 56
-        val description: String = java.lang.String.valueOf(R.string.descriptionstring) + desc_id
+        //val desc_id = 56
+        //val description: String = java.lang.String.valueOf(R.string.descriptionstring) + desc_id
 
         //itemsDeclaration();
         val nameTextView = findViewById<TextView>(R.id.product_name)
@@ -30,7 +52,35 @@ class MainActivity : AppCompatActivity() {
         priceTag.text = price.toString()
         val img: ImageView = findViewById(R.id.imageView2)
         img.setImageResource(image)
-        btnAddtoCart = findViewById(R.id.buttoncart)
+        val btnAddCart = findViewById<Button>(R.id.buttoncart)
 
+        val pendingIntent = TaskStackBuilder.create(this).run {
+            addNextIntentWithParentStack(intent)
+            getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+
+        val notif = NotificationCompat.Builder(this,CHANNEL_ID)
+            .setContentTitle("You bought $name")
+            .setContentText("Just for $price . Enjoy ya cheeky bastard!")
+            .setStyle(NotificationCompat.BigTextStyle()
+                .bigText("Just for $price . Enjoy ya cheeky bastard!\nSale in the next week... Catch up"))
+            .setLargeIcon(BitmapFactory.decodeResource(getResources(), image))
+            .setStyle(NotificationCompat.BigPictureStyle()
+                .bigPicture(BitmapFactory.decodeResource(getResources(), image))
+                .bigLargeIcon(null))
+            .setSmallIcon(R.drawable.notification_icon)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .build()
+
+
+        val notifManger = NotificationManagerCompat.from(this)
+
+
+        // onClick listener for the button
+        btnAddCart.setOnClickListener {
+            notifManger.notify(NOTIF_ID,notif)
+
+        }
     }
 }
